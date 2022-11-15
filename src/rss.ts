8a5261e772990dd69898672935b2e73a2fe69339
chenyc <flyforest@outlook.com>
2022-11-15 16:00:59 +0800

    update

--- src/rss.ts
+++ src/rss.ts
@@ -1,4 +1,4 @@
 import * as RSSHub from 'rsshub';
 
 RSSHub.init({ CACHE_TYPE: null });
-RSSHub.request(`/weibo/user/2622472937/`).then((res) => console.log(res)).catch(e => console.log(e));
+RSSHub.request(`/twitter/user/VitalikButerin/excludeReplies=1&count=3`).then((res) => console.log(res)).catch(e => console.log(e));
