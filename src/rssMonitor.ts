1f61b4bfc8481a7f9807297983486640cc117e10
chenyc <flyforest@outlook.com>
2022-11-16 18:34:10 +0800

    update

--- src/rssMonitor.ts
+++ src/rssMonitor.ts
@@ -5,6 +5,7 @@ import { get_dynamic_config, static_config } from './config';
 import { sendMessage } from './wxClinet';
 import { sleep } from './utils';
 
+const runtime = Date.now();
 const cached = new Set();
 const parseRss = (rssData: any) => {
     const datas = rssData.item
@@ -34,12 +35,12 @@ const parseRss = (rssData: any) => {
     return datas;
 };
 
-const getRss = async (url: string, time: number, firstTime: boolean) => {
+const getRss = async (url: string) => {
     console.log('开始获取RSS', url);
     try {
-        const rssData = await RSSHub.request(url).then((res) => parseRss(res));
+        const rssData = await RSSHub.request(url).then((res: any) => parseRss(res));
         console.log(rssData)
-        if (!firstTime) {
+        if (Date.now() - runtime > 1000 * 60) { // 启动的一分钟内不推送
             for (const item of rssData) {
                 const id = uuid.v4();
                 const url = `http://tw.ccxx.icu/${id}.html`;
@@ -54,20 +55,20 @@ const getRss = async (url: string, time: number, firstTime: boolean) => {
         console.log(e);
     }
 
-    await sleep(time);
-    getRss(url, time, false);
+    await sleep(10 * 1000);
+    getRss(url);
 }
 
 RSSHub.init({
     CACHE_TYPE: null,
 });
 const run = async () => {
-    getRss('/twitter/user/cz_binance/excludeReplies=1&count=3', 1000 * 10, true);
-    getRss('/twitter/user/elonmusk/excludeReplies=1&count=3', 1000 * 10, true);
-    // getRss('/twitter/user/binancezh/excludeReplies=1&count=3', 1000 * 10, true);
-    getRss('/twitter/user/VitalikButerin/excludeReplies=1&count=3', 1000 * 10, true);
-    getRss('/twitter/user/SBF_FTX/excludeReplies=1&count=3', 1000 * 10, true);
-    // getRss('/weibo/user/2622472937/', 1000 * 60);
+    getRss('/twitter/user/cz_binance/excludeReplies=1&count=3');
+    getRss('/twitter/user/elonmusk/excludeReplies=1&count=3');
+    // getRss('/twitter/user/binancezh/excludeReplies=1&count=3');
+    getRss('/twitter/user/VitalikButerin/excludeReplies=1&count=3');
+    getRss('/twitter/user/SBF_FTX/excludeReplies=1&count=3');
+    // getRss('/weibo/user/2622472937/');
 };
 
 export {
